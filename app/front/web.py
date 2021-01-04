96611bee093d201af197c39649c43f0781ec5655
Matheus Abreu <matheus.abreu@olist.com>
2021-01-04 17:19:15 -0300

    Created flask application and routes

---
+++ app/front/web.py
@@ -0,0 +1,43 @@
+import sys
+from flask import Flask, render_template, request, redirect, flash
+
+sys.path.append('app')
+
+from back.marketplaces import create_marketplace
+from back.products import create_product
+
+
+app = Flask(__name__)
+app.secret_key = 'IOAHGFYAOGFEYHAGO'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/marketplace')
+def marketplace_form():
+    return render_template('new_marketplace.html')
+
+@app.route('/marketplace/create')
+def marketplace_create():
+    name = request.args.get('name')
+    description = request.args.get('description')
+    create_marketplace(name, description)
+    flash(f'Marketplace Created! - {name}')
+    return redirect('/')
+
+@app.route('/product')
+def product_form():
+    return render_template('new_product.html')
+
+@app.route('/product/create')
+def product_create():
+    name = request.args.get('name')
+    description = request.args.get('description')
+    price = float(request.args.get('price'))
+    create_product(name, description, price)
+    flash(f'Product Created! - {name}')
+    return redirect('/')
+
+app.run(debug=True)
