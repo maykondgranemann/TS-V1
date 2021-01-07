8aa78aab8d7653c414de042845bfbdc59d915520
Matheus Lima Andrade da Silva <matheus@Matheuss-MacBook-Air.local>
2021-01-07 10:10:27 -0300

    added category

---
+++ app/back/categories.py
@@ -0,0 +1,20 @@
+from .utils import write_to_logs, write_to_txt_store, read_txt
+
+
+def create_category(name: str, description: str):
+    write_to_txt_store(f'Name: {name} - Desc: {description}', 'categories')
+    write_to_logs(f'Created Category - {name}')
+
+def read_categories() -> list:
+    list_categories = read_txt('categories')
+    list_formatted = []
+    for line in list_categories:
+        marketplace = []
+        lista_dados_linha = line.split('-')
+        name = lista_dados_linha[0].strip('Name: ')
+        descr = lista_dados_linha[1].strip('Desc: ')
+        marketplace.append(name)
+        marketplace.append(descr)
+        list_formatted.append(marketplace)
+    write_to_logs('List Categories')
+    return list_formatted
--- app/front/web.py
+++ app/front/web.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template, request, redirect, flash
 
-
-
+from app.back.categories import create_category, read_categories
 from app.back.marketplaces import create_marketplace, read_marketplaces
 from app.back.products import create_product, read_products
-
 
 app = Flask(__name__)
 app.secret_key = 'IOAHGFYAOGFEYHAGO'
@@ -28,10 +26,11 @@ def marketplace_create():
     flash(f'Marketplace Created! - {name}')
     return redirect('/')
 
+
 @app.route('/marketplace/list')
 def marketplace_read():
     list_marketplaces = read_marketplaces()
-    return render_template('read_marketplaces.html', list = list_marketplaces)
+    return render_template('list_marketplaces.html', list=list_marketplaces)
 
 
 @app.route('/product')
@@ -48,8 +47,29 @@ def product_create():
     flash(f'Product Created! - {name}')
     return redirect('/')
 
+
 @app.route("/product/list")
 def list_products():
     product_list = read_products()
-    
+
     return render_template("list_products.html", product_list=product_list)
+
+
+@app.route('/category')
+def category_form():
+    return render_template('new_category.html')
+
+
+@app.route('/category/create')
+def category_create():
+    name = request.args.get('name')
+    description = request.args.get('description')
+    create_category(name, description)
+    flash(f'Category Created! - {name}')
+    return redirect('/')
+
+
+@app.route('/category/list')
+def category_read():
+    list_categories = read_categories()
+    return render_template('list_categories.html', list=list_categories)
