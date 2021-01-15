b4800e2620f26f23ef4a62c0acc0c521856bd332
Lucas Torres <75676431+lucastorresolist@users.noreply.github.com>
2021-01-15 13:05:51 -0300

    Removed comments in ProductController

--- app/back/controllers/product_controller.py
+++ app/back/controllers/product_controller.py
@@ -6,23 +6,3 @@ class ProductController(BaseController):
         self.name_entity = name_entity
         self.__dao = ProductDao()
         super().__init__(self.__dao, self.name_entity)
-
-
-
-# def create_product(product: Product) -> None:
-#     set_product(product)
-#     create_log(f"Created product {product.name}")
-
-# def read_products() -> list:
-#     products = get_products()
-#     create_log(f"Listing products")
-#     return products
-
-
-# def update_product(product: Product) -> None:
-#     upd_product(product)
-#     create_log(f"Updated product")
-
-# def delete_product(id: str)-> None:
-#     del_product(id)
-#     create_log(f"Deleted product")
