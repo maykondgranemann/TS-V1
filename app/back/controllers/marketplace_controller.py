663023a818834ec4ce9e7e0e37f0b03d44761014
Lucas Torres <75676431+lucastorresolist@users.noreply.github.com>
2021-01-15 13:07:19 -0300

    Removed comments in MarketplaceController

--- app/back/controllers/marketplace_controller.py
+++ app/back/controllers/marketplace_controller.py
@@ -8,20 +8,3 @@ class MarketplaceController(BaseController):
         self.name_entity = name_entity
         self.__dao = MarketplaceDao()
         super().__init__(self.__dao, self.name_entity)
-
-# def create_marketplace(marketplace: Marketplace) -> None:
-#     set_marketplace(marketplace)
-#     create_log(f"Created marketplace {marketplace.name}")
-
-# def read_marketplace() -> list:
-#     marketplaces = get_marketplace()
-#     create_log(f"Listing marketplaces")
-#     return marketplaces
-
-# def delete_marketplace(id: str):
-#     del_marketplace(id)
-#     create_log(f"Deleting marketplace")
-
-# def update_marketplace(marketplace: Marketplace):
-#     upd_marketplace(marketplace)
-#     create_log(f"Deleting marketplaces")
