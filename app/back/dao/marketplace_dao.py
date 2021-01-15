62f1bb3c5e4dead758ae540cdf43c8625e60cb94
Lucas Torres <75676431+lucastorresolist@users.noreply.github.com>
2021-01-15 13:48:24 -0300

    Removed white lines

--- app/back/dao/marketplace_dao.py
+++ app/back/dao/marketplace_dao.py
@@ -9,13 +9,11 @@ class MarketplaceDao(BaseDao):
                     ('{model.name}', '{model.description}'); """
         super().execute(query)
 
-
     def read_by_id(self, id: int) -> Marketplace:
         query = f"SELECT id, name, description FROM marketplace WHERE id = {id};"
         result = super().read(query)[0]
         marketplace = Marketplace(result[1], result[2], result[0])
         return marketplace
-
 
     def read_all(self) -> list:
         query = f"SELECT id, name, description FROM marketplace;"
@@ -26,11 +24,9 @@ class MarketplaceDao(BaseDao):
             marketplaces.append(marketplace)
         return marketplaces
 
-
     def delete(self, id:int)->None:
             query = f"DELETE FROM marketplace WHERE id = {id};"
             super().execute(query)
-
 
     def update(self, model: Marketplace) -> None:
         query = f"UPDATE marketplace SET name = '{model.name}', description = '{model.description}' WHERE ID = {model.id};"
