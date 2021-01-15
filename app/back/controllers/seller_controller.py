a929aef84e14f03a9ef7cbcab98fccde95dbccc1
Lucas Torres <75676431+lucastorresolist@users.noreply.github.com>
2021-01-15 13:05:00 -0300

    Removed comments

--- app/back/controllers/seller_controller.py
+++ app/back/controllers/seller_controller.py
@@ -8,20 +8,3 @@ class SellerController(BaseController):
         self.name_entity = name_entity
         self.__dao = SellerDao()
         super().__init__(self.__dao, self.name_entity)
-
-# def create_seller(seller: Seller) -> None:
-#     set_seller(seller)
-#     create_log(f"Created seller {seller.name}")
-
-# def read_seller() -> list:
-#     sellers = get_seller()
-#     create_log(f"Listing sellers")
-#     return sellers
-
-# def delete_seller(id: str):
-#     del_seller(id)
-#     create_log(f"Listing sellers")
-
-# def update_seller(seller: Seller):
-#     upd_seller(seller)
-#     create_log(f"Deleting sellers")
