ad00027ce63e2f3801290747c3847f99c85951c6
Lucas Torres <75676431+lucastorresolist@users.noreply.github.com>
2021-01-15 13:44:52 -0300

    Removed import and white line

--- app/back/controllers/seller_controller.py
+++ app/back/controllers/seller_controller.py
@@ -1,9 +1,7 @@
 from app.back.controllers.base_controller import BaseController
-# from app.back.controllers.log_controller import create_log
 from app.back.dao.seller_dao import SellerDao
 
 class SellerController(BaseController):
-
     def __init__(self, name_entity):
         self.name_entity = name_entity
         self.__dao = SellerDao()
