1cb44a8a389420b96953274af008e5f52647e650
Lucas Torres <75676431+lucastorresolist@users.noreply.github.com>
2021-01-15 13:42:29 -0300

    Removed white line

--- app/back/controllers/category_controller.py
+++ app/back/controllers/category_controller.py
@@ -2,7 +2,6 @@ from app.back.controllers.base_controller import BaseController
 from app.back.dao.category_dao import CategoryDao
 
 class CategoryController(BaseController):
-
     def __init__(self, name_entity):
         self.name_entity = name_entity
         self.__dao = CategoryDao()
