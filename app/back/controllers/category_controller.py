23cf0f63751f1618e849f7977d049377b9a1fa16
Lucas Torres <75676431+lucastorresolist@users.noreply.github.com>
2021-01-15 12:59:34 -0300

    Removed comments

--- app/back/controllers/category_controller.py
+++ app/back/controllers/category_controller.py
@@ -7,22 +7,3 @@ class CategoryController(BaseController):
         self.name_entity = name_entity
         self.__dao = CategoryDao()
         super().__init__(self.__dao, self.name_entity)
-
-    # def create_category(category: Category) -> None:
-    #     super().create(category)
-    #     create_log(f"Created category {category.name}")
-
-    # def read_categories() -> list:
-    #     result = super().read()
-    #     create_log(f"Listing categories")
-    #     return result
-
-    # def update_category(category: Category) -> None:
-    #     super().update(category)
-    #     create_log(f"Updated category {category.name}")
-
-    # def delete_category(id: str):
-    #     super().delete(id)
-    #     create_log(f"Deleted category {id}")
-
-
