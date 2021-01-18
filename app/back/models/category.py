5b755b164623d90b2d28a4c147b2ea64df71c3e5
Matheus Lopes <matheusblopes10@gmail.com>
2021-01-18 11:54:23 -0300

    Métodos de acesso para os models de log, category e sellers

--- app/back/models/category.py
+++ app/back/models/category.py
@@ -1,6 +1,30 @@
 class Category:
 
     def __init__(self, name: str, description: str, id: int = None) -> None:
-        self.id = id
-        self.name = name
-        self.description = description
+        self.__id = id
+        self.__name = name
+        self.__description = description
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    @property
+    def description(self):
+        return self.__description
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    @name.setter
+    def name(self, name)
+        self.__name = name
+    
+    @description.setter
+    def description(self, description)
+        self.__description = description
+    
+    @id.setter
+    def id(self, id)
+        self.__id = id
--- app/back/models/log.py
+++ app/back/models/log.py
@@ -1,6 +1,30 @@
 class Log:
 
     def __init__(self, date: str, message: str, id: int = None):
-        self.date = date
-        self.message = message
-        self.id = id
+        self.__date = date
+        self.__message = message
+        self.__id = id
+
+    @property
+    def date(self):
+        return self.__date
+    
+    @property
+    def message(self):
+        return self.__message
+    
+    @property
+    def id(self):
+        return self.__id
+
+    @date.setter
+    def date(self, date):
+        self.__date = date
+    
+    @message.setter
+    def message(self, message):
+        self.__message = message
+    
+    @id.setter
+    def id(self, id):
+        self.__id = id
