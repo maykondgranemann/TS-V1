81aacc4fa77c3f6ef0cb6d8d5761d2957ff72ab4
Juliana Karoline <julianaklulo@gmail.com>
2021-01-19 14:21:23 -0300

    Create base controller

---
+++ backend/controllers/base_controller.py
@@ -0,0 +1,21 @@
+from backend.daos.base_dao import BaseDao
+
+
+class BaseController:
+    def __init__(self, dao):
+        self.__dao = dao
+
+    def create(self, model: object) -> None:
+        return self.__dao.save(model)
+
+    def read_by_id(self, id: int) -> object:
+        return self.__dao.read_by_id(id)
+
+    def read_all(self) -> list:
+        return self.__dao.read_all()
+
+    def delete(self, model: object) -> None:
+        self.__dao.delete(model)
+
+    def update(self, model: object) -> None:
+        self.__dao.save(model)
