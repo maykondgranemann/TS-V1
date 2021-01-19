cf62bde51e9addae15bc3e3e9e96716d6570aa01
Ralph <ralph.souza@olist.com>
2021-01-19 13:35:46 +0000

    Refatoração de código.

--- dao/dao_base.py
+++ dao/dao_base.py
@@ -1,17 +1,30 @@
+# Importar as características da BaseModel
 from ..model.model_base import BaseModel
+
+# Importar a classe Session em session.py
+from .session import Session
 
 # Criar classe base para a DAO 
 class DaoBase:
+    def __init__(self, type_model):
+        self.__type_model = type_model
+
     def save(self, model:BaseModel) -> None:
-        self.__session.add(model)
-        self.__session.commit()
+        with Session() as session:
+            session.add(model)
+            session.commit()
 
     def read_all(self) -> list:
-        return self.__session.query(BaseModel).all()
+        with Session() as session:
+            result = session.query(self.__type_model).all()
+        return result
 
     def read_by_id(self, id_:int) -> BaseModel:
-        return self.__session(BaseModel).filter_by(id=id).first()
+        with Session() as session:
+            result = session(self.__type_model).filter_by(id=id_).first()
+        return result
 
     def delete(self, model: BaseModel) -> None:
-        self.__session.delete(model)
-        self.__session.commit()
+        with Session() as session:
+            session.delete(model)
+            session.commit()
