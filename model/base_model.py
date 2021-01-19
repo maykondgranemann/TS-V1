a1db52dbf2ce93ffa034440f6549df49150f3fe4
Quesia <quesiasts@gmail.com>
2021-01-19 16:17:39 -0300

    requirements txt

--- model/base_model.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer
-
-Base = declarative_base()
-
-class BaseModel(Base):
-    __abstract__ = True
-    id = Column(Integer, primary_key = True)
