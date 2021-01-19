1804ece865291347bb2b46a68a951a29272c3003
Ralph <ralph.souza@olist.com>
2021-01-19 13:16:08 +0000

    Criação das estruturas do CRUD.

--- dao/dao_seller.py
+++ dao/dao_seller.py
@@ -1,16 +1,21 @@
 # Importar a da model a model_seller
 from ..model.model_seller import Seller
 
+# Importar a função da sessão através do session.py
+from .session import Session
+
 # Criar uma classe para acessar o BD 
 class SellerDao:
-    def save(self, model:Seller):
-        pass
+    def save(self, model:Seller) -> None:
+        self.__session.add(model)
+        self.__session.commit()
 
     def read_all(self) -> list:
-        pass
+        return self.__session.query(Seller).all()
 
     def read_by_id(self, id_:int) -> Seller:
-        pass
+        return self.__session(Seller).filter_by(id=id).first()
 
-    def delete(self, id_:int) -> None:
-        pass
+    def delete(self, model: Seller) -> None:
+        self.__session.delete(model)
+        self.__session.commit()
