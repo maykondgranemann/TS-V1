9271a2d1b419781cbd0bda6a6f795c7726fe9e8d
Juliana Karoline <julianaklulo@gmail.com>
2021-01-19 14:20:55 -0300

    Create base DAO

---
+++ backend/daos/base_dao.py
@@ -0,0 +1,27 @@
+from backend.models.base_model import BaseModel
+from backend.daos.session import Session
+
+
+class BaseDao:
+    def __init__(self, type_model):
+        self.__type_model = type_model
+
+    def save(self, model:BaseModel) -> None:
+        with Session() as session:
+            session.add(model)
+            session.commit()
+
+    def read_all(self) -> list:
+        with Session() as session:
+            result = session.query(self.__type_model).order_by('id').all()
+        return result
+
+    def read_by_id(self, id:int) -> BaseModel:
+        with Session() as session:
+            result = session.query(self.__type_model).filter_by(id=id).first()
+        return result
+
+    def delete(self, model:BaseModel)-> None:
+        with Session() as session:
+            session.delete(model)
+            session.commit()
