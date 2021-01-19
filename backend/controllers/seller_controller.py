ec0953b06802e00ed76f6008e5f4465f0a105d16
josue-avila <josue.avila@olist.com>
2021-01-19 13:41:03 -0300

    implementing base controller

--- backend/controllers/seller_controller.py
+++ backend/controllers/seller_controller.py
@@ -1,21 +1,8 @@
 from backend.dao.seller_dao import SellerDao
-from backend.models.base_model import BaseModel
+from backend.controllers.base_controller import BaseController
 
 
-class SellerController:
+class SellerController(BaseController):
     def __init__(self):
-        self.dao = SellerDao()
+        super().__init__(SellerDao)
 
-    def save(self, model: BaseModel) -> None:
-        self.dao.save(model)
-
-    def read_all(self) -> list:
-        result = self.dao.read_all()
-        return result
-
-    def read_by_id(self, id: int) -> object:
-        result = self.dao.read_by_id(id)
-        return result
-
-    def delete(self, model: BaseModel) -> None:
-        self.dao.delete(model)
