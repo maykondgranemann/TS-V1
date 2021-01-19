e37eb8dc34cbbe2f91883d60ea3e3ea4bcc06583
Derli Machado <dam.machado1@gmail.com>
2021-01-19 14:42:35 -0300

    Add: run.

--- run.py
+++ run.py
@@ -1,1 +1,22 @@
-from backend.controller.base_controller import BaseController
+import sys
+sys.path.append('.')
+
+from controller.seller_controller import SellerController
+from models.seller_model import Seller
+
+seller = Seller('Ada', 'a@a.com', '11-111111')
+
+# create/update
+# SellerController().save(seller)
+
+# read all
+sellers = SellerController().read_all()
+for seller in sellers:
+    print(seller.name)
+
+# delete
+#SellerController().delete(sellers[1])
+
+
+
+
