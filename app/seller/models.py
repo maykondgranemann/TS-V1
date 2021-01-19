21d0ce80bd570a55ccfdd2d80a1160c9310833d1
Matheus Abreu <matheus.abreu@olist.com>
2021-01-19 13:57:47 -0300

    Created Seller model

---
+++ app/seller/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, String
+
+from app.database.models import BaseModel
+
+
+class Seller(BaseModel):
+    __tablename__ = 'seller'
+    fullname = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
+
+    def __init__(self, fullname: str, email: str, phone: str):
+        self.fullname = fullname
+        self.email = email
+        self.phone = phone
