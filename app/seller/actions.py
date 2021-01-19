f81691dbac87556ddc1046a3e7ae28c7cd02e753
Matheus Abreu <matheus.abreu@olist.com>
2021-01-19 13:58:14 -0300

    Created Seller actions

---
+++ app/seller/actions.py
@@ -0,0 +1,36 @@
+from typing import List
+
+from app.seller.dao import SellerDao
+from app.seller.exceptions import SellerNotFoundException
+from app.seller.models import Seller
+
+DAO = SellerDao()
+
+
+def create(fullname: str, email: str, phone: str) -> None:
+    model = Seller(fullname, email, phone)
+    DAO.save(model)
+
+
+def read_all() -> List[Seller]:
+    return DAO.read_all()
+
+
+def read_by_id(id: int) -> Seller:
+    result = DAO.read_by_id(id)
+    if result is None:
+        raise SellerNotFoundException
+    return result
+
+
+def update(fullname: str, email: str, phone: str, id: int) -> None:
+    model = read_by_id(id)
+    model.fullname = fullname
+    model.email = email
+    model.phone = phone
+    DAO.save(model)
+
+
+def delete(id: int) -> None:
+    model = read_by_id(id)
+    DAO.delete(model)
