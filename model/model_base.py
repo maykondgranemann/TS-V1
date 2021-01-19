1954f475d6c8479f6e80e82e08735826c97fec78
Ralph <ralph.souza@olist.com>
2021-01-19 10:43:59 +0000

    Criação do repositório de models, com base e seller, e primeira classe com definição de ID como parâmetro comum entre models.

---
+++ model/model_base.py
@@ -0,0 +1,17 @@
+# importar Column e Integer
+# importar declarative_base
+from sqlalchemy import Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+# Criar variável que receba a função declarative_base
+Base = declarative_base()
+
+# A model_base deverá conter qualquer informação que possa ser 
+# comum em outras models, neste caso o ID
+class BaseModel(Base):
+
+# Criar o abstract para corrigir conflito de tabela
+    __abstract__ = True
+    
+# Declarar a ID como coluna no BD
+    id_ = Column(Integer, primary_key=True)
