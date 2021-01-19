98d60962f904bec618f36391d7e743de443aefd5
Lucas Dario <lucas.edu.u@hotmail.com>
2021-01-19 16:26:43 -0300

    Inclusão do virtual env no projeto

--- dao/seller_dao.py
+++ dao/seller_dao.py
@@ -1,5 +1,3 @@
-import sys
-sys.path.append('.')
 from model.seller_model import Seller
 from dao.base_dao import BaseDao
 
@@ -17,8 +15,8 @@ class SellerDao(BaseDao):
 # SellerDao().save(obj_seller)
 
 ##TEST-LIST ALL##
-# for item in SellerDao().read_all():
-#     print(item.id, item.name, item.phone, item.email)
+for item in SellerDao().read_all():
+    print(item.id, item.name, item.phone, item.email)
 
 # TEST-LIST BY ID
 # result = SellerDao().read_by_id(13)
