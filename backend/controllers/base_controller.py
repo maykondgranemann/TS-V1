ba8817c455581bc92c57e1b5cdf1ebd949b9c86c
Juliana Karoline <julianaklulo@gmail.com>
2021-01-20 18:20:04 -0300

    Fix type hint on base controller

--- backend/controllers/base_controller.py
+++ backend/controllers/base_controller.py
@@ -1,11 +1,11 @@
 from backend.daos.base_dao import BaseDao
-
+from backend.models.base_model import BaseModel
 
 class BaseController:
     def __init__(self, dao):
         self.__dao = dao
 
-    def create(self, model: object) -> None:
+    def create(self, model: BaseModel) -> None:
         return self.__dao.save(model)
 
     def read_by_id(self, id: int) -> object:
@@ -14,8 +14,8 @@ class BaseController:
     def read_all(self) -> list:
         return self.__dao.read_all()
 
-    def delete(self, model: object) -> None:
+    def delete(self, model: BaseModel) -> None:
         self.__dao.delete(model)
 
-    def update(self, model: object) -> None:
+    def update(self, model: BaseModel) -> None:
         self.__dao.save(model)
