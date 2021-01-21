c3a0e812ffc6fb3ba529228cc6e7c5d460de1f51
Ralph <ralph.souza@olist.com>
2021-01-21 21:18:24 +0000

    Criação de teste para inserção de dados da controller_base.

--- test/controller/test_controller_base.py
+++ test/controller/test_controller_base.py
@@ -1,1 +1,19 @@
+import sys
+sys.path.append('.')
 
+from controller.controller_base import BaseController
+
+
+# Testando inserção de dados via Controller_base
+def test_should_save():
+
+    name = 'Americanas'
+    email = 'contato@americanas.com'
+    phone = '11 999999999'
+
+    seller2 = BaseController(name, email, phone)
+
+    dao = BaseController()
+    dao.save(seller2)
+
+    assert isinstance(seller2, BaseController)
