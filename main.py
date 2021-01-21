dfa5ad638bb2d52b1dff2b8092b0eff5db7a4d36
jcosta86 <jeferson.costa@olist.com>
2021-01-21 19:43:07 -0300

    criando interação para transações no terminal

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import sys
 sys.path.append('.')
 
 from back.controllers.tests_controller import start_tests
+from back.models.seller import Seller
+from back.controllers.seller_controller import SellerController
+
 
 def menu():
     options = ['Cadastrar seller', 'Listar todos os sellers', 'Buscar seller por id', 
@@ -18,21 +21,46 @@ def menu():
 while True:
     try:
         op = menu()
+        controller = SellerController()
         if op == 1:
-            # new_seller()
-            pass
+            name = input("Informe o nome do seller: ")
+            phone = input("Informe o telefone: ")
+            email = input("Informe o e-mail: ")
+            seller = Seller(name, phone, email)
+            controller.save(seller)
+            print(f'\nSeller {name} salvo.')
         elif op == 2:
-            # list_seller()
-            pass
+            result = controller.read_all()
+            for r in result:
+                print(f'id: {r.id} - Nome: {r.name} - Telefone: {r.phone} - E-mail: {r.email}')
         elif op == 3:
-            # read_by_id()
-            pass
+            id = int(input("Informe o id: "))
+            seller = controller.read_by_id(id)
+            print(f'Nome: {seller.name} - Telefone: {seller.phone} - E-mail: {seller.email}')
         elif op == 4:
-            # product_detail()
-            pass
+            id = int(input("Informe o id: "))
+            seller = controller.read_by_id(id)
+            print(seller.name, seller.phone, seller.email)
+            seller.name = input('Informe o novo nome:')
+            seller.phone = input('Informe o novo telefone:')
+            seller.email = input('Informe o novo email:')
+            controller.save(seller)
+            print(f'Seller atualizado.')
         elif op == 5:
-            # delete_product()
-            pass
+            id = int(input("Informe o id: "))
+            seller = controller.read_by_id(id)
+            print(f"""
+                  Deseja realmente excluir?
+                  Seller: {seller.name}
+                  Telefone: {seller.phone}
+                  E-mail: {seller.email}
+                  """)
+            check = int(input('[1] - Confirmar\n[2] - Calcelar: '))
+            if check == 1:
+                controller.delete(seller)
+                print('\nExcluido...') 
+            else:
+                print('Cancelado')
         elif op == 6:
             start_tests()
         elif op == 7:
@@ -40,4 +68,4 @@ while True:
         else:
             print('Opção indisponível. Tente novamente.')
     except ValueError:
-        print('Opção indisponível. Tente novamente.')
+        print('Opção precisa ser um numero inteiro listado.')
