2a424be7b3d8a1a16c68134b3fb1889f17d84873
Matheus Lopes <matheus.bachiste@olist.com>
2021-01-21 13:32:07 -0300

    Updated base dao

--- app/back/dao/base_dao.py
+++ app/back/dao/base_dao.py
@@ -1,21 +1,32 @@
-from app.back.dao.connection import Connection
+from .session import Session
+from ..models.base_model import BaseModel
+
 
 class BaseDao:
-    def execute(self, query: str) -> None:
-        try:
-            with Connection() as connection:
-                cursor = connection.cursor()
-                cursor.execute(query)
-                connection.commit()
-        except Exception as error:
-            print(error)
+    def __init__(self, type_model) -> None:
+        self.type_model = type_model
+
+    def save(self, model: BaseModel) -> None:
+        with Session() as session:
+            session.add(model)
+            session.commit()
+
+    def update(self, model: BaseModel) -> None:
+        with Session() as session:
+            session.add(model)
+            session.commit()
+
+    def read_all(self) -> list:
+        with Session() as session:
+            result = session.query(self.type_model).all()
+        return result
+
+    def read_by_id(self, id: int) -> BaseModel:
+        with Session() as session:
+            result = session.query(self.type_model).filter_by(id=id).first()
+        return result    
     
-    def read(self, query: str) -> list:
-        try:
-            with Connection() as connection:
-                cursor = connection.cursor()
-                cursor.execute(query)
-                result = cursor.fetchall()
-            return result
-        except Exception as error:
-            print(error)
+    def delete(self, model: BaseModel) -> None:
+        with Session() as session:
+            session.delete(model)
+            session.commit()
