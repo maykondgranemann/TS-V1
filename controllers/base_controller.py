19087a380c7c67d57b5213c6cacf8efdc040246f
olgaleao <olgaleao@gmail.com>
2021-01-21 14:10:03 -0400

    criacao de controllers

---
+++ controllers/base_controller.py
@@ -0,0 +1,18 @@
+from models.base_model import BaseModel
+
+
+class BaseController:
+    def __init__(self, dao):
+        self.__dao = dao
+
+    def save(self, model: BaseModel) -> None:
+        self.__dao.save(model)
+
+    def read_by_id(self, id: int) -> BaseModel:
+        return self.__dao.read_by_id(id)
+
+    def read_all(self) -> list:
+        return self.__dao.read_all()
+
+    def delete(self, model: BaseModel) -> None:
+        self.__dao.delete(model)
