9405db4b7a8b8e1c5825ef1dc33aa29c4185d106
jcosta86 <jeferson.costa@olist.com>
2021-01-21 18:44:32 -0300

    implementando função para iniciar testes em dao

--- back/tests/dao/test_base_dao.py
+++ back/tests/dao/test_base_dao.py
@@ -8,36 +8,32 @@ from back.dao.session import Session
 
 
 class TestBaseDao:
+    base_dao = BaseDao(Seller)
+    object_name = "!@#$%¨$*&//"
+    object_phone = '123'
+    object_email = 'ze@ze.com'
+    
     
     def test_instantiate_a_class(self) -> None:
-         test_object = BaseDao(Seller)
-         assert isinstance(test_object, BaseDao)
+         assert isinstance(self.base_dao , BaseDao)
          
     def test_save(self) -> None:
-        test = BaseDao(Seller)
-        object_name = "!@#$%¨$*&//"
-        object_phone = '123'
-        object_email = 'ze@ze.com'
-        s = Seller(object_name, object_phone, object_email)
-        test.save(s)
+        s = Seller(self.object_name, self.object_phone, self.object_email)
+        self.base_dao.save(s)
         with Session() as session:
-            model = session.query(Seller).filter_by(name=object_name).first()
+            model = session.query(Seller).filter_by(name=self.object_name).first()
         object_db_name = model.name
-        assert object_db_name == object_name
-        test.delete(model)
+        assert object_db_name == self.object_name
+        self.base_dao.delete(model)
               
     def test_delete(self) -> None:
-        test = BaseDao(Seller)
-        object_name = "!@#$%¨$*&//"
-        object_phone = '123'
-        object_email = 'ze@ze.com'
-        s = Seller(object_name, object_phone, object_email)
-        test.save(s)
+        s = Seller(self.object_name, self.object_phone, self.object_email)
+        self.base_dao.save(s)
         with Session() as session:
-            model = session.query(Seller).filter_by(name=object_name).first()
-        test.delete(model)
+            model = session.query(Seller).filter_by(name= self.object_name).first()
+        self.base_dao.delete(model)
         with Session() as session:
-            model_after_delete = session.query(Seller).filter_by(name=object_name).first()
+            model_after_delete = session.query(Seller).filter_by(name=self.object_name).first()
         assert model_after_delete is None
     
     def test_save_string(self) -> None:
@@ -53,36 +49,45 @@ class TestBaseDao:
             assert isinstance(e, TypeError) 
     
     def test_read_all(self) -> None:
-        seller_test = BaseDao(Seller)
-        result = seller_test.read_all()
+        result = self.base_dao.read_all()
         if len(result) > 0:
             assert isinstance(result, list)
         else:
             print("Database is empty")
     
     def test_read_by_id_using_an_integer_id(self) -> None:
-        seller_test = BaseDao(Seller)
-        result = seller_test.read_by_id(4)
+        result = self.base_dao.read_by_id(4)
         assert isinstance(result, Seller)
 
     def test_read_by_id_using_an_string_id(self) -> None:
-        seller_test = BaseDao(Seller)
         try:
-            result = seller_test.read_by_id('string')
+            result = self.base_dao.read_by_id('string')
         except Exception as e:
             assert isinstance(e, NameError)  
 
     def test_delete_using_an_string(self) -> None:
-        seller_test = BaseDao(Seller)
         try:
-            seller_test.delete('string')
+            self.base_dao.delete('string')
         except Exception as e:
             assert isinstance(e, NameError) 
     
     def test_delete_using_an_integer(self) -> None:
-        seller_test = BaseDao(Seller)
         try:
-            seller_test.delete(1)
+            self.base_dao.delete(1)
         except Exception as e:
             assert isinstance(e, NameError)      
 
+
+def start_base_dao_tests():
+    test_base_dao = TestBaseDao()
+    test_base_dao.test_instantiate_a_class()
+    test_base_dao.test_save()
+    test_base_dao.test_delete()
+    test_base_dao.test_save_string()
+    test_base_dao.test_save_integer()
+    test_base_dao.test_read_all()
+    test_base_dao.test_read_by_id_using_an_integer_id()
+    test_base_dao.test_read_by_id_using_an_string_id()
+    test_base_dao.test_delete_using_an_string()
+    test_base_dao.test_delete_using_an_integer()
+  
--- back/tests/dao/test_seller_dao.py
+++ back/tests/dao/test_seller_dao.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('.')
 
 from back.dao.seller_dao import SellerDao
+from back.dao.base_dao import BaseDao
 
 
 class TestSellerDao:
@@ -9,3 +10,7 @@ class TestSellerDao:
     def test_instance(self):
         seller_dao_instance = SellerDao()
         assert isinstance(seller_dao_instance, BaseDao)
+
+def start_test_seller_dao():
+    test_seller_dao = TestSellerDao()
+    test_seller_dao.test_instance()
