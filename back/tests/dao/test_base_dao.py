53e9ab672f083f2749aba65741ebea6b28395ba7
jcosta86 <jeferson.costa@olist.com>
2021-01-21 16:42:51 -0300

    criando metodos de teste de save e delete em base dao

--- back/tests/dao/test_base_dao.py
+++ back/tests/dao/test_base_dao.py
@@ -4,26 +4,55 @@ sys.path.append('.')
 from back.dao.base_dao import BaseDao
 from back.models.seller import Seller
 from back.dao.seller_dao import SellerDao
+from back.dao.session import Session
+
 
 class TestBaseDao:
     
-    def test_instantiate_a_class(self):
+    def test_instantiate_a_class(self) -> None:
          test_object = BaseDao(Seller)
          assert isinstance(test_object, BaseDao)
-      
-    def test_save_string(self):
+         
+    def test_save(self) -> None:
+        test = BaseDao(Seller)
+        object_name = "!@#$%¨$*&//"
+        object_phone = '123'
+        object_email = 'ze@ze.com'
+        s = Seller(object_name, object_phone, object_email)
+        test.save(s)
+        with Session() as session:
+            model = session.query(Seller).filter_by(name='!@#$%¨$*&//').first()
+        object_db_name = model.name
+        assert object_db_name == object_name
+        test.delete(model)
+              
+    def test_delete(self) -> None:
+        test = BaseDao(Seller)
+        object_name = "!@#$%¨$*&//"
+        object_phone = '123'
+        object_email = 'ze@ze.com'
+        s = Seller(object_name, object_phone, object_email)
+        test.save(s)
+        with Session() as session:
+            model = session.query(Seller).filter_by(name='!@#$%¨$*&//').first()
+        test.delete(model)
+        with Session() as session:
+            model_after_delete = session.query(Seller).filter_by(name='!@#$%¨$*&//').first()
+        assert model_after_delete is None
+    
+    def test_save_string(self) -> None:
         try:
             BaseDao.save('string')
         except Exception as e:
             assert isinstance(e, TypeError)        
     
-    def test_save_integer(self):
+    def test_save_integer(self) -> None:
         try:
             BaseDao.save(1)
         except Exception as e:
             assert isinstance(e, TypeError) 
     
-    def test_read_all(self):
+    def test_read_all(self) -> None:
         seller_test = BaseDao(Seller)
         result = seller_test.read_all()
         if len(result) > 0:
@@ -31,28 +60,28 @@ class TestBaseDao:
         else:
             print("Database is empty")
     
-    def test_read_by_id_using_an_integer_id(self):
+    def test_read_by_id_using_an_integer_id(self) -> None:
         seller_test = BaseDao(Seller)
         result = seller_test.read_by_id(4)
         assert isinstance(result, Seller)
 
-    def test_read_by_id_using_an_string_id(self):
+    def test_read_by_id_using_an_string_id(self) -> None:
         seller_test = BaseDao(Seller)
         try:
             result = seller_test.read_by_id('string')
         except Exception as e:
             assert isinstance(e, NameError)  
 
-    def test_delete_using_an_string(self):
+    def test_delete_using_an_string(self) -> None:
         seller_test = BaseDao(Seller)
         try:
             seller_test.delete('string')
         except Exception as e:
             assert isinstance(e, NameError) 
     
-    def test_delete_using_an_integer(self):
+    def test_delete_using_an_integer(self) -> None:
         seller_test = BaseDao(Seller)
         try:
             seller_test.delete(1)
         except Exception as e:
-            assert isinstance(e, NameError)           
+            assert isinstance(e, NameError)      
