bb2e4be272bc0e5a7084ef935197fcb04053c1f0
Ralph <ralph.souza@olist.com>
2021-01-21 21:17:05 +0000

    Criação de teste para inserção de dados da model_seller.

--- test/model/test_model_seller.py
+++ test/model/test_model_seller.py
@@ -3,12 +3,17 @@ sys.path.append('.')
 
 from model.model_seller import Seller
 
+# Testando inserção de atributos da model_seller
+def test_should_save():
 
-name = "B2W"
-email = "contato@seller.com"
-phone = "(11)9 9999-9999"
+    name = "B2W"
+    email = "contato@seller.com"
+    phone = "(11)9 9999-9999"
 
-seller1 = Seller(name, email, phone)
+    seller1 = Seller(name, email, phone)
 
-# Testando objeto da classe Seller
-assert isinstance(seller1, Seller)
+    dao = Seller()
+    dao.save(seller1)
+
+    # Testando objeto da classe Seller
+    assert isinstance(seller1, Seller)
