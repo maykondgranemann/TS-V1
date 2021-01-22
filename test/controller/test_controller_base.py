c1eb1adbbe04dc8775f5b4c0712bd73c478d860b
Ralph <ralph.souza@olist.com>
2021-01-22 10:52:27 +0000

    Refatoração de código e testes com uso de classe.

--- test/controller/test_controller_base.py
+++ test/controller/test_controller_base.py
@@ -2,18 +2,57 @@ import sys
 sys.path.append('.')
 
 from controller.controller_base import BaseController
+from model.model_seller import Seller
+from dao.session import Session
 
 
 # Testando inserção de dados via Controller_base
-def test_should_save():
+class TestBaseController:
 
-    name = 'Americanas'
-    email = 'contato@americanas.com'
-    phone = '11 999999999'
+    name = 'Só Por Deus!!!'
+    email = 'contato@sopordeus!!!.com'
+    phone = '(11) 9 6666-6666'
+    id = None
 
-    seller2 = BaseController(name, email, phone)
+    dao = BaseController(Seller)
 
-    dao = BaseController()
-    dao.save(seller2)
+    def test_should_save(self):
 
-    assert isinstance(seller2, BaseController)
+        seller = BaseController(self.name, self.email, self.phone)
+        self.dao.save(seller)
+        with Session() as session:
+            test = session.query(Seller).filter_by(name = self.name).first()
+
+        assert self.name == test
+        self.id = test
+
+    def test_should_read_all(self):
+        seller = self.dao.read_all()
+
+        assert isinstance(seller, list)
+
+    def test_should_read_by_id(self):
+        with Session() as session:
+            test = session.query(Seller).filter_by(id = self.id).first()
+        seller = self.dao.read_by_id(id = test)
+
+        assert isinstance(seller, Seller)
+
+    def test_should_delete(self):
+        with Session() as session:
+            test = session.query(Seller).filter_by(id = self.id).first()
+        seller = self.dao.read_by_id(id = self.id)
+
+        self.dao.delete(seller)
+        seller_deleted = self.dao.read_by_id(id = self.id)
+
+        assert seller_deleted == None
+
+def start_controllerbase_test():
+
+    TestBaseController().test_should_save()
+    TestBaseController().test_should_read_all()
+    TestBaseController().test_should_read_by_id()
+    TestBaseController().test_should_delete()
+
+start_controllerbase_test()
