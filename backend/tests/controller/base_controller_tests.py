d5d31fa88b95eb65a1e46ac8004e85b89e13980e
rafaela.teixeira <rafaellynha.a@hotmail.com>
2021-01-22 14:57:06 -0300

    tests update_delete

--- backend/tests/controller/base_controller_tests.py
+++ backend/tests/controller/base_controller_tests.py
@@ -26,5 +26,39 @@ def test_create():
     assert phone == r.phone
     assert email == r.email
     
+def test_delete(): 
+    base = BaseController(_dao)
+    seller = Seller('Rafaela', '4199862-8989', 'rafa@teste.com')
+    
+    base.save(seller)
+    r = base.read_all()[-1]
+    s = base.read_by_id(r.id)
+    assert base.delete(s) == None
+    
+    
+def test_update(): 
+    base = BaseController(_dao)
+    seller = Seller('Rafaela', '4199862-8989', 'rafa@teste.com')
+    
+    base.save(seller)
+    result = base.read_all()[-1]
+    r = base.read_by_id(result.id)
+    name = 'Teixeira Rafaela'
+    email = '419987678997'
+    phone = 'rafa@rafaela.com'
+    r.name = name
+    r.phone = phone
+    r.email = email
+    id = r.id
+    
+    base.save(r)
+    s = base.read_by_id(id)
+    
+    assert name == s.name
+    assert email == s.email
+    assert phone == s.phone    
+    
 #test_read_all()    
 #test_create()
+#test_delete()
+#test_update()
--- backend/tests/dao/base_dao_tests.py
+++ backend/tests/dao/base_dao_tests.py
@@ -33,6 +33,32 @@ def test_delete():
     s = dao.read_by_id(r.id)
     assert dao.delete(s) == None
     
+    
+def test_update(): 
+    dao = BaseDao(Seller)
+    seller = Seller('Rafaela', '4199862-8989', 'rafa@teste.com')
+    
+    dao.save(seller)
+    result = dao.read_all()[-1]
+    r = dao.read_by_id(result.id)
+    name = 'Teixeira Rafaela'
+    email = '419987678997'
+    phone = 'rafa@rafaela.com'
+    r.name = name
+    r.phone = phone
+    r.email = email
+    id = r.id
+    
+    dao.save(r)
+    s = dao.read_by_id(id)
+    
+    assert name == s.name
+    assert email == s.email
+    assert phone == s.phone
+    
+    
+    
 #test_read_all()
 #test_create()   
-test_delete()
+#test_delete()
+#test_update()
