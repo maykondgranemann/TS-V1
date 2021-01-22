ff46fe835afc7cb2cc31b21e454e53affa2ffd43
Lucas Dario <lucas.edu.u@hotmail.com>
2021-01-22 09:43:54 -0300

    Incluso testes para os métodos de delete em dao e controller

--- tests/controller/seller_test.py
+++ tests/controller/seller_test.py
@@ -30,14 +30,17 @@ def test_method_create():
     control = create_instace()
     obj = create_obj()
     result = control.save(obj)
+    assert result == None, '[In Control]O resultado de control.save não foi o esperado'
 
     list_messy = control.read_all()
+    assert isinstance(
+        list_messy, list), '[In Control]A lista de retorno de control.read_all não é do tipo esperado'
+
     list_ordered = sorted(list_messy, key=lambda k: k.id)
     obj = list_ordered[-1]
 
     assert isinstance(
         obj, Seller), '[In Control]O objeto de retorno de control.read_all não é o esperado'
-    assert result == None, '[In Control]O resultado de control.save não foi o esperado'
     assert obj.name == name_, '[In Control]O resultado de obj.name não é satisfatório'
     assert obj.phone == phone_, '[In Control]O resultado de obj.phone não é satisfatório'
     assert obj.email == email_, '[In Control]O resultado de obj.email não é satisfatório'
@@ -56,11 +59,20 @@ def test_method_update(obj: Seller):
     obj.phone = phone2
     obj.email = email2
     result = control.save(obj)
-    obj_update = control.read_by_id(id_temp)
-
     assert result == None, '[In control]O resultado de control.save não foi o esperado'
+
+    obj_update = control.read_by_id(id_temp)
     assert isinstance(
         obj_update, Seller), '[In control]O objeto de retorno de control.read_by_id não é o esperado'
     assert obj_update.name == name2, '[In control]O resultado de obj_update.name não é satisfatório'
     assert obj_update.phone == phone2, '[In control]O resultado de obj_update.phone não é satisfatório'
     assert obj_update.email == email2, '[In control]O resultado de obj_update.email não é satisfatório'
+    test_method_delete(obj_update)
+
+
+def test_method_delete(obj: Seller):
+    control = create_instace()
+    id_temp = obj.id
+    obj_delete = control.read_by_id(id_temp)
+    result = control.delete(obj_delete)
+    assert result == None, '[In control]O resultado de control.delete não foi o esperado'
--- tests/dao/seller_test.py
+++ tests/dao/seller_test.py
@@ -20,7 +20,8 @@ def create_obj():
 
 def create_instace():
     dao = SellerDao()
-    assert isinstance(dao, SellerDao), '[In Dao]Instância não é do tipo esperado'
+    assert isinstance(
+        dao, SellerDao), '[In Dao]Instância não é do tipo esperado'
     return dao
 
 
@@ -28,14 +29,17 @@ def test_method_create():
     dao = create_instace()
     obj = create_obj()
     result = dao.save(obj)
+    assert result == None, '[In Dao]O resultado de dao.save não foi o esperado'
 
     list_messy = dao.read_all()
+    assert isinstance(
+        list_messy, list), '[In Dao]A lista de retorno de dao.read_all não é do tipo esperado'
+
     list_ordered = sorted(list_messy, key=lambda k: k.id)
     obj = list_ordered[-1]
 
     assert isinstance(
         obj, Seller), '[In Dao]O objeto de retorno de dao.read_all não é o esperado'
-    assert result == None, '[In Dao]O resultado de dao.save não foi o esperado'
     assert obj.name == name_, '[In Dao]O resultado de obj.name não é satisfatório'
     assert obj.phone == phone_, '[In Dao]O resultado de obj.phone não é satisfatório'
     assert obj.email == email_, '[In Dao]O resultado de obj.email não é satisfatório'
@@ -54,11 +58,20 @@ def test_method_update(obj: Seller):
     obj.phone = phone2
     obj.email = email2
     result = dao.save(obj)
-    obj_update = dao.read_by_id(id_temp)
-
     assert result == None, '[In Dao]O resultado de dao.save não foi o esperado'
+
+    obj_update = dao.read_by_id(id_temp)
     assert isinstance(
         obj_update, Seller), '[In Dao]O objeto de retorno de dao.read_by_id não é o esperado'
     assert obj_update.name == name2, '[In Dao]O resultado de obj_update.name não é satisfatório'
     assert obj_update.phone == phone2, '[In Dao]O resultado de obj_update.phone não é satisfatório'
     assert obj_update.email == email2, '[In Dao]O resultado de obj_update.email não é satisfatório'
+    test_method_delete(obj_update)
+
+
+def test_method_delete(obj: Seller):
+    control = create_instace()
+    id_temp = obj.id
+    obj_delete = control.read_by_id(id_temp)
+    result = control.delete(obj_delete)
+    assert result == None, '[In Dao]O resultado de dao.delete não foi o esperado'
